08790a6179e91647d848f3a8730fcbaf18f1db32
Ludmila Dombrowska <vanilla77177@gmail.com>
2023-02-22 16:01:30 +0100

    feat: Implemented brackets resolution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  let opening = {}
+  let closing = {}
+  bracketsConfig.forEach((c) => {
+      opening[c[0]] = c[1]
+      closing[c[1]] = c[0]
+  })
+
+  for (let i = 0; i < str.length; i++) {
+      let c = str[i]
+
+      if (
+          closing[c] !== undefined &&
+          stack.length > 0 &&
+          stack.at(-1) === closing[c]
+      ) {
+          stack.pop()
+      } else if (opening[c] !== undefined) {
+          stack.push(c)
+      } else {
+          return false
+      }
+  }
+
+  return stack.length == 0
 }
